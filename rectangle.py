ffbaf11e133bc96984d8f37ad0165d3299736319
Marcel Suter <marcel.suter@bzz.ch>
2026-09-03 08:45:55 +0200

    added description for __init__

--- rectangle.py
+++ rectangle.py
@@ -1,2 +1,4 @@
 class Rectangle:
-    pass
+    def __init__(self):  # Ergänze die Parameter
+        # TODO Falls width oder height kleiner oder gleich 0 ist
+        raise ValueError("width and height must be greater than 0")
